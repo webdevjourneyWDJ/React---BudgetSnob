e0bc88fe8cd81bb465242989296344e1ecad9aba
Gamaliel Barron <gbarronmm@gmail.com>
2020-01-05 07:49:29 -0800

    added Link and NavLink components

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import "./styles/styles.scss";
 
 const BudgetSnobHome =() => (
@@ -15,12 +15,29 @@ const BudgetEdit =() => (
     <div>Budget Snob Edit</div>
 )
 
+const PageNotFound = () =>(
+    <div>404! - <Link to="/">Home</Link></div>
+)
+
+const Header = () => (
+    <header>
+        <h1>Budget Snob</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+        <NavLink to="/add" activeClassName="is-active">ADD</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">EDIT</NavLink>
+    </header>
+)
+
 const routes = (
     <BrowserRouter>
         <div>
-           <Route path="/" component={BudgetSnobHome} exact={true}/>
-           <Route path="/add" component={BudgetAdd}/>
-           <Route path="/edit" component={BudgetEdit}/>
+            <Header />
+            <Switch>
+                <Route path="/" component={BudgetSnobHome} exact={true}/>
+                <Route path="/add" component={BudgetAdd}/>
+                <Route path="/edit" component={BudgetEdit}/>
+                <Route component={PageNotFound}/>
+            </Switch>
         </div>
     </BrowserRouter>
 );
